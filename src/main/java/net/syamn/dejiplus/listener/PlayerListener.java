7eb7ea6edd514b268a253d041973a1a69744f6ca
syamn <admin@sakura-server.net>
2013-01-10 06:16:11 +0900

    Add GeoIP

---
+++ src/main/java/net/syamn/dejiplus/listener/PlayerListener.java
@@ -0,0 +1,13 @@
+/**
+ * dejiplus - Package: net.syamn.dejiplus.listener
+ * Created: 2013/01/10 6:11:35
+ */
+package net.syamn.dejiplus.listener;
+
+/**
+ * PlayerListener (PlayerListener.java)
+ * @author syam(syamn)
+ */
+public class PlayerListener {
+
+}
